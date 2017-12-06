19863f5d6b9d4a6f99a75de2bf22db7ece949de9
Bohdan-Ivan Maliuha <bogdan.maliuga@gmail.com>
2017-12-07 01:12:21 +0200

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
@@ -9,13 +9,15 @@ import { AppRoutingModule } from './/app-routing.module';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { MainComponent } from './components/main/main.component';
 import { ApiService } from './services/api.service';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    MainComponent
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
